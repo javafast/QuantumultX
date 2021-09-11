811be37ed37c6af7daaddb7f519bf6d82f87de60
javafast <35760655+javafast@users.noreply.github.com>
2021-09-11 15:32:36 +0800

    Update productDetail.js

--- ggszhg/productDetail.js
+++ ggszhg/productDetail.js
@@ -7,9 +7,12 @@ var body = $response.body;
             let product =list[j];
             let prodcutName = product["name"];
             if (product["isSellOut"]) {
-                $notify("贵高速", "温馨提示", prodcutName+"卖完了")
+                //$notify("贵高速", "温馨提示", prodcutName+"卖完了")
             }
             let nowPrice = parseInt(product["integral"]);
+            if (nowPrice >1680) {
+                $notify("贵高速", "温馨提示", prodcutName+"价格大于1680")
+            }
             let top = parseInt("1400");
             console.log("------||商品价格||----："+ nowPrice)
             let needCount =Math.ceil(top/nowPrice);
@@ -18,9 +21,7 @@ var body = $response.body;
             }
             product["name"]= "("+needCount+")"+prodcutName;
             product["goodsSpec"] = product["goodsSpec"] + "----" + (needCount * nowPrice);
-            if (nowPrice >1680) {
-                $notify("贵高速", "温馨提示", prodcutName+"价格大于1680")
-            }
+          
            //console.log(JSON.stringify(product))
         }
         body = JSON.stringify(daJSON);
