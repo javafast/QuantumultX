368ce8eeca85151f5676148987a289d2b7a8ea6e
javafast <35760655+javafast@users.noreply.github.com>
2021-09-11 14:45:52 +0800

    Update productDetail.js
    优化页面

--- ggszhg/productDetail.js
+++ ggszhg/productDetail.js
@@ -1,25 +1,27 @@
 var body = $response.body;
    try {   
+        console.log("进入商品明细")
         var daJSON = JSON.parse(body);
         var list = daJSON.data;        
         for (var j=0;  j < list.length; j++) {
             let product =list[j];
+            if (product["isSellOut"]) {
+                $notify("贵高速", "温馨提示", "卖完了")
+            }
             let nowPrice = parseInt(product["integral"]);
             let top = parseInt("1400");
-            var count =  product["stock"];
-            if (parseInt(count) ==0) {
-                $notify("贵高速", "温馨提示", "商品数量为0")
-            }
-            console.log("------||商品数量||----："+ count)
+            console.log("------||商品价格||----："+ nowPrice)
             let prodcutName = product["name"];
             let needCount =Math.floor(top/nowPrice);
-            product["proName"]= "("+count+">"+needCount+")"+prodcutName;
-            if (parseInt(product["salePrice"]) >168000) {
-                $notify("贵高速", "温馨提示", "价格大于168000")
+            product["name"]= "("+count+">"+needCount+")"+prodcutName;
+            product["goodsSpec"] = product["goodsSpec"] + "----" + (needCount * nowPrice);
+            if (nowPrice >1680) {
+                $notify("贵高速", "温馨提示", prodcutName+"价格大于1680")
             }
            //console.log(JSON.stringify(product))
         }
         body = JSON.stringify(daJSON);
+        console.log("商品明细-完成")
         $done(body)
     } catch(e) {
         console.log(e)
