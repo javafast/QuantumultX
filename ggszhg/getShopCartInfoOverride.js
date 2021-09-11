9ab17b49915c90b948e1e71e11631da26d855a1d
javafast <35760655+javafast@users.noreply.github.com>
2021-09-11 17:03:01 +0800

    Update getShopCartInfoOverride.js
    baocuo

--- ggszhg/getShopCartInfoOverride.js
+++ ggszhg/getShopCartInfoOverride.js
@@ -6,11 +6,11 @@
         var list = tv.products;
         var i =0;
         for (var j=0;  j < list.length; j++) {
+            let productNow =list[j];
+            let product = productNow["product"];
             if (parseInt(product["marketPrice"]) >16800000) {
                  $notify("贵高速", "温馨提示", "价格大于1680")
             }
-            let productNow =list[j];
-            let product = productNow["product"];
             let nowPrice = parseInt(product["marketPrice"]);
             let top = parseInt("14000000");
             let prodcutName = product["productName"];
