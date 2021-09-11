ec0e0c20102cb11b2bf063aca768f40d3e09c461
javafast <35760655+javafast@users.noreply.github.com>
2021-09-11 14:12:23 +0800

    Create getShopCartInfoOverride.js
    添加购物车计算数量

---
+++ ggszhg/getShopCartInfoOverride.js
@@ -0,0 +1,25 @@
+ var body = $response.body;
+   try {   
+        var daJSON = JSON.parse(body);
+        var tv = daJSON.data;
+        var list = tv.products;
+        var i =0;
+        for (var j=0;  j < list.length; j++) {
+            let productNow =list[j];
+            let product = productNow["product"];
+            let nowPrice = parseInt(product["marketPrice"]);
+            let top = parseInt("140000");
+            let prodcutName = product["productName"];
+            let needCount =Math.floor(top/nowPrice);
+            product["productName"]= "("+count+">"+needCount+")"+prodcutName;
+            if (parseInt(product["marketPrice"]) >168000) {
+                 $notify("贵高速", "温馨提示", "价格大于1680")
+            }
+           console.log(JSON.stringify(product))
+        }
+        body = JSON.stringify(daJSON);
+        $done(body)
+    } catch(e) {
+        console.log(e)
+        $done(body)
+    }
