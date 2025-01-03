d95105714ae34c4d24a0c7408ef44ce612f08690
javafast <35760655+javafast@users.noreply.github.com>
2025-01-03 15:30:37 +0800

    Update 51yd.js

--- xmly/51yd.js
+++ xmly/51yd.js
@@ -3,26 +3,18 @@
 console.log("开始修改数量")
 var body = $response.body;
  try {   
-      var daJSON = JSON.parse(body);
-      var data = daJSON.data;
-      var list = data.stocks;
-      var i =0;
-      for (var j=0;  j < list.length; j++) {
-        let productNow =list[j];
-        var name = productNow["stockName"];
-        var total = productNow["total"];
-        var sent = productNow["sent"];
-        let result = Number(total) - Number(sent);
-        console.log(name)
-        console.log(result)
-        if (result === 0) {
-          productNow["sent"] = total;
-            console.log("修改结束"+productNow["sent"])
+      var obj = JSON.parse(body);
+      obj.data.stocks.forEach(stock => { 
+        console.log(stock.stockName)
+        var total = Number(stock.total);
+        var sent = Number(stock.sent);
+        if (total - sent === 0) {
+           console.log("修改："+name)
+           stock.sent = 0;
         }
-      }
-      console.log("修改结束")
-      body = JSON.stringify(daJSON);
-      $done(body)
+     });
+      body = JSON.stringify(obj);
+      $done({ body });
 } catch(e) {
       console.log(e)
       $done(body)
