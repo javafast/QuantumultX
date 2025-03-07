f001921c7329305234b891a69be82b7f6c8971d5
javafast <35760655+javafast@users.noreply.github.com>
2025-03-07 17:05:43 +0800

    Update cityLuckCoffee.js

--- xmly/cityLuckCoffee.js
+++ xmly/cityLuckCoffee.js
@@ -1,16 +1,19 @@
-console.log("瑞幸抢购校验");
+console.log("开始修改返回体");
 var body = $response.body;
 try {
     var obj = JSON.parse(body);
-    if (obj.success !== undefined ) {
-       obj.success = true
-       obj.code = "0"
-        console.log("修改瑞幸状态");
+
+    // 仅在 code = "-1" 且 success = false 时修改
+    if (obj.code === "-1") {
+        console.log("检测到库存售罄，修改返回值...");
+        obj.code = "0";  
+        obj.success = true;
     }
-    
+
     body = JSON.stringify(obj);
+    console.log(obj)
     $done({ body });
-} catch(e) {
-    console.log(e);
+} catch (e) {
+    console.log("解析 JSON 失败:", e);
     $done(body);
 }
