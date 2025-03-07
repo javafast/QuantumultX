c615f4f4c8ffecbabfbad5a2e9bb38b676ddf331
javafast <35760655+javafast@users.noreply.github.com>
2025-03-07 16:53:25 +0800

    Update cityLuckCoffee.js

--- xmly/cityLuckCoffee.js
+++ xmly/cityLuckCoffee.js
@@ -4,6 +4,8 @@ try {
     var obj = JSON.parse(body);
     if (obj.success !== undefined ) {
        obj.success = true
+       obj.code = "0"
+        console.log("修改瑞幸状态");
     }
     
     body = JSON.stringify(obj);
