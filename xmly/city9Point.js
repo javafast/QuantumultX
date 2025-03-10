e1a7911a10d4d4a379b6870ef8029bf3e675da75
javafast <35760655+javafast@users.noreply.github.com>
2025-03-10 13:54:31 +0800

    Update city9Point.js

--- xmly/city9Point.js
+++ xmly/city9Point.js
@@ -2,13 +2,16 @@ console.log("修改中信九积分库存");
 var body = $response.body;
 try {
     var obj = JSON.parse(body);
-
-    // 仅在 stockNum = 0 时修改
-    if (obj.data && obj.data.stockNum === 0) {
-        console.log("检测到库存售罄，修改返回值...");
-        obj.data.stockNum = 1000;
+    console.log("修改中信九积分库存");
+    if (obj.data) {
+        log.info("库存"+obj.data.stockNum)
+         // 仅在 stockNum = 0 时修改
+        if (obj.data.stockNum == 0) {
+            console.log("检测到库存售罄，修改返回值...");
+            obj.data.stockNum = 1000;
+        }
     }
-
+   
     body = JSON.stringify(obj);
     $done({ body });
 } catch (e) {
