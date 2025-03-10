a0145cfbc0e3f6e30e2314b50921dbee2f0125d9
javafast <35760655+javafast@users.noreply.github.com>
2025-03-10 13:51:03 +0800

    Update city9Point.js

--- xmly/city9Point.js
+++ xmly/city9Point.js
@@ -3,15 +3,15 @@ var body = $response.body;
 try {
     var obj = JSON.parse(body);
 
-    // 仅在 code = "-1" 且 success = false 时修改
-    if (obj.data.stockNum === "0") {
+    // 仅在 stockNum = 0 时修改
+    if (obj.data && obj.data.stockNum === 0) {
         console.log("检测到库存售罄，修改返回值...");
-        bj.data.stockNum = 1000
+        obj.data.stockNum = 1000;
     }
 
     body = JSON.stringify(obj);
     $done({ body });
 } catch (e) {
     console.log("解析 JSON 失败:", e);
-    $done(body);
+    $done({ body });
 }
